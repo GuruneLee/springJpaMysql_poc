5c7188b93ffadaf63d89545bbfb5a2ba94cf0590
ChangHa Lee <dlckdgk4858@gmail.com>
2024-05-20 21:21:18 +0900

    init

---
+++ src/main/java/com/gurunelee/lockpoc/LockPocApplication.java
@@ -0,0 +1,13 @@
+package com.gurunelee.lockpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockPocApplication.class, args);
+    }
+
+}
